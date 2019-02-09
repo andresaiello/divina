431b63f97cfd7f52ee86188a5e5fd231e49afe0e
Lucas <ljanon@gmail.com>
2019-02-09 19:19:10 -0300

    Fix loading screen

--- components/shared/components/LoadingScreen.jsx
+++ components/shared/components/LoadingScreen.jsx
@@ -6,26 +6,24 @@ import Loader from './Loader';
 import withMainLayout from '~/HOCs/withMainLayout';
 
 const FullScreenLoader = styled.div`
-  position: absolute;
+  position: fixed;
   width: 100%;
   height: 100%;
   color: white;
   background: white;
   z-index: 100000;
+  touch-action: none;
 `;
 
 export default function LoadingScreen ({ withLayout }) {
+  const loader = (
+    <FullScreenLoader>
+      <Loader />
+    </FullScreenLoader>
+  );
   const Loading = withLayout
-    ? withMainLayout(() => (
-      <FullScreenLoader>
-        <Loader />
-      </FullScreenLoader>
-    ))
-    : () => (
-      <FullScreenLoader>
-        <Loader />
-      </FullScreenLoader>
-    );
+    ? withMainLayout(() => loader)
+    : () => loader;
 
   return <Loading />;
 }
