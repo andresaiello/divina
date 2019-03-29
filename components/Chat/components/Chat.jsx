08ffaa3490f5a20346d3cf5fac18eb276047ef44
Andres Aiello <andres.aiello@fravega.com.ar>
2019-03-29 15:47:18 -0300

    fix chat vh

--- components/Chat/components/Chat.jsx
+++ components/Chat/components/Chat.jsx
@@ -17,8 +17,8 @@ const StyledChat = styled.div`
     background-size: cover;
     display: flex;
     flex-direction: column;
-    /* height: 100vh; */
-    min-height: -webkit-fill-available;
+    height: 100vh;
+    /* min-height: -webkit-fill-available; */
     max-height: -webkit-fill-available;
   }
 `;
