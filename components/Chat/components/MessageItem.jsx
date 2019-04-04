d375e63ea2e602788be1f239a1d39672a36eea7e
Andres Aiello <andres.aiello@fravega.com.ar>
2019-04-04 19:07:19 -0300

    feat chat styles

--- components/Chat/components/MessageItem.jsx
+++ components/Chat/components/MessageItem.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import Avatar from './Avatar';
 import timeAgo from '~/lib/timeAgo';
+import format from 'date-fns/format';
 import SecContext from '~/context/secContext';
 
 const StyledMessageItem = styled.li`
@@ -11,6 +12,7 @@ const StyledMessageItem = styled.li`
     padding-top: 8px;
     padding-bottom: 8px;
     list-style: none;
+    opacity: 0.9;
 
     .author {
       background: #fff;
@@ -22,7 +24,7 @@ const StyledMessageItem = styled.li`
     .content{
       flex-grow: 1;
       height: 100%;
-      margin: 0 27px 27px 27px;
+      margin: 0 27px 13px 27px;
       background-color: #dcf8c6;
       border-radius: 7.5px;
       opacity: 0.8;
@@ -30,14 +32,18 @@ const StyledMessageItem = styled.li`
     
     .createdAt {
       position: absolute;
-      right: 30px;
-      top: 0;
+      right: 35px;
+      bottom: 23px;
+      color: rgb(74, 74,74);
+      font-size: 12px;
     }
 
     .avatar{
       width: 30px;
       height: 30px;
-      top: -10px;
+      top: 0;
+      left: 7px;
+      margin: 0;
       position: absolute;
       z-index: 1;
     }
@@ -46,7 +52,7 @@ const StyledMessageItem = styled.li`
       text-align: right;
 
       .avatar{
-        right: 0;
+        right: 7px;
         left: initial;
       }
 
@@ -54,11 +60,11 @@ const StyledMessageItem = styled.li`
         padding: 0 35px 0 0;
       }
       .content{
-        background-color: #fff;
+        background-color: rgb(248, 231, 28);
       }
       .createdAt {
         right: initial;
-        left: 30px;
+        left: 35px;
       }
     }
 
@@ -78,15 +84,17 @@ const MessageItem = class extends React.PureComponent {
         createdAt, content, author, author: { username },
       },
     } = this.props;
+    const created = format(new Date(parseInt(createdAt, 10)), 'HH:mm');
+
 
     return (
       <StyledMessageItem>
-        <div className={(username === user.username) ? 'self' : null}>
+        <div className={(username === user.username) ? 'self' : 'friend'}>
           <Avatar user={author} />
           <div className="content">
             <div className="author">{username}</div>
             <div className="content">{content}</div>
-            <div className="createdAt">{timeAgo.format(parseInt(createdAt, 10))}</div>
+            <div className="createdAt">{created}</div>
           </div>
         </div>
       </StyledMessageItem>
--- components/Chat/components/MessageList.jsx
+++ components/Chat/components/MessageList.jsx
@@ -5,6 +5,7 @@ import MessageItem from './MessageItem';
 const StyledMessageList = styled.ul`
   && {
     padding-top: 20px;
+    margin-bottom: 40px;
     flex: 1;
     overflow-y: scroll;
     padding: 8px 0 8px 0;
